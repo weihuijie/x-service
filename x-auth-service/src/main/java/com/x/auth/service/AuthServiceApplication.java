7427616053f50afd25b504fae5858f58ee445890
whj-w <>
2025-10-27 13:32:37 +0800

    apigateway  403 问题处理

--- x-auth-service/src/main/java/com/x/auth/service/AuthServiceApplication.java
+++ x-auth-service/src/main/java/com/x/auth/service/AuthServiceApplication.java
@@ -1,12 +1,9 @@
 package com.x.auth.service;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration;
 
-@SpringBootApplication(exclude = {GrpcServerAutoConfiguration.class})
-@EnableDubbo
+@SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
